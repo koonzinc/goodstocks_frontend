79d9d39620adea44a81bb7f28df0dd81f9da882a
koonzinc <koonz.eth@gmail.com>
2022-11-04 13:07:29 -0400

    update

--- src/components/SocialProof.jsx
+++ src/components/SocialProof.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SocialProof = () => {
   return (
     <section class="bg-white  ">
-      <div class="max-w-[70%] px-4 py-8 mx-auto text-center lg:py-16 lg:px-6 bg-white rounded-lg shadow-2xl mb-20 ">
+      <div class="max-w-[60%] px-4 py-8 mx-auto text-center lg:py-16 lg:px-6 bg-white rounded-lg shadow-2xl mb-20 ">
         <dl class="grid max-w-screen-md gap-8 mx-auto  sm:grid-cols-3 ">
           <div class="flex flex-col items-center justify-center">
             <dt class="mb-2 text-3xl md:text-4xl font-extrabold text-[#2752FF]">
