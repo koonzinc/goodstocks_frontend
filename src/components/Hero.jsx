31d10a5f470242501fb251c6e88ac2a0e3d4a828
koonzinc <koonz.eth@gmail.com>
2022-11-07 19:10:47 -0500

    update link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -32,27 +33,31 @@ const Hero = () => {
           You've come to the right place
         </h1>
         <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">
-          Here at Goodstocks we focus on 5 stocks. Not because they are the best, but because they are good. We also can't afford the paid plan for our API.
+          Here at Goodstocks we focus on 5 stocks. Not because they are the
+          best, but because they are good. We also can't afford the paid plan
+          for our API.
         </p>
         <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-          <a
-            href="#"
-            class="inline-flex justify-center border hover:bg-gray-100 border-gray-300 items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 "
-          >
-            Enter app
-            <svg
-              class="ml-2 -mr-1 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+          <Link to='/app-home' replace>
+            <a
+              href="#"
+              class="inline-flex justify-center border hover:bg-gray-100 border-gray-300 items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 "
             >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
+              Enter app
+              <svg
+                class="ml-2 -mr-1 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </Link>
           <a
             href="#"
             class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 "
@@ -71,10 +76,7 @@ const Hero = () => {
         <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
           <span class="font-semibold text-gray-400 uppercase">FEATURED IN</span>
           <div class="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
-            <a
-              href="#"
-              class="mr-5 mb-5 lg:mb-0 hover:text-gray-800 "
-            >
+            <a href="#" class="mr-5 mb-5 lg:mb-0 hover:text-gray-800 ">
               <svg
                 class="h-8"
                 viewBox="0 0 132 29"
@@ -119,10 +121,7 @@ const Hero = () => {
                 />
               </svg>
             </a>
-            <a
-              href="#"
-              class="mr-5 mb-5 lg:mb-0 hover:text-gray-800 "
-            >
+            <a href="#" class="mr-5 mb-5 lg:mb-0 hover:text-gray-800 ">
               <svg
                 class="h-11"
                 viewBox="0 0 208 42"
@@ -143,10 +142,7 @@ const Hero = () => {
                 />
               </svg>
             </a>
-            <a
-              href="#"
-              class="mr-5 mb-5 lg:mb-0 hover:text-gray-800 "
-            >
+            <a href="#" class="mr-5 mb-5 lg:mb-0 hover:text-gray-800 ">
               <svg
                 class="h-11"
                 viewBox="0 0 120 41"
