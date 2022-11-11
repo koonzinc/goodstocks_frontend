8bbd34fcb1e5e0520140e1ef3cb3da764a06a586
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-11-11 16:14:07 -0500

    tfw

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../components/FrontPage/Navbar";
-
+import axios from "../axios";
 
 const ForgotPassword = () => {
+  const [emailSent, setEmailSent] = useState(false);
+  const [codeVerified, setCodeVerified] = useState(false);
+  const [email, setEmail] = useState("");
+  const [code, setCode] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleEmail = (e) => {
+    e.preventDefault();
+    console.log(email);
+    axios
+      .post("reset-password", {
+        email: email,
+      })
+      .then((res) => setEmailSent(true))
+      .catch((err) => console.log("Error", err));
+  };
+  const handleCode = (e) => {
+    e.preventDefault();
+    axios
+      .post("verify-code", {
+        password_code: code,
+      })
+      .then((res) => setCodeVerified(true))
+      .catch((err) => console.log("Error", err));
+  };
+  const handlePassword = (e) => {
+    e.preventDefault();
+    axios
+      .put("change-password", {
+        password_code: code,
+        email: email,
+        password: newPassword,
+      })
+      .then((res) => navigate("/signin"))
+      .catch((err) => console.log("Error", err));
+  };
+
   return (
     <>
       <Navbar />
@@ -18,7 +57,12 @@ const ForgotPassword = () => {
             <h2 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
               Change Password
             </h2>
-            <form class="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+
+            <form
+              onSubmit={handleEmail}
+              class="mt-4 space-y-4 lg:mt-5 md:space-y-5"
+              action="#"
+            >
               <div>
                 <label
                   for="email"
@@ -28,37 +72,62 @@ const ForgotPassword = () => {
                 </label>
                 <input
                   type="email"
-                  name="email"
                   id="email"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                   placeholder="name@company.com"
-                  required=""
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div>
-                <label
-                  for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
-                >
-                  New Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                  required=""
-                />
-              </div>
-
-              <button
-                type="submit"
-                class="w-full text-white bg-[#2752FF] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-              >
-                Reset password
-              </button>
             </form>
+            {emailSent && (
+              <form onSubmit={handleCode}>
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    code
+                  </label>
+                  <input
+                    type="text"
+                    id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                    placeholder="code"
+                    required
+                    onChange={(e) => setCode(e.target.value)}
+                  />
+                </div>
+              </form>
+            )}
+            {codeVerified && (
+              <form onSubmit={handlePassword}>
+                <div>
+                  <label
+                    for="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    New Password
+                  </label>
+                  <input
+                    onChange={(e) => setNewPassword(e.target.value)}
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                    required
+                  />
+                </div>
+              </form>
+            )}
+
+            <button
+              type="submit"
+              class="w-full text-white bg-[#2752FF] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+            >
+              Reset password
+            </button>
           </div>
         </div>
       </section>
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../components/FrontPage/Navbar";
+import axios from "../axios";
 
 const AppLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("login", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        console.log("API response: ", res);
+        localStorage.setItem("userToken", res.data);
+        navigate("/app-home");
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+
   return (
     <>
       <Navbar />
@@ -19,7 +42,11 @@ const AppLogin = () => {
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                 Sign in to your account
               </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
+              <form
+                class="space-y-4 md:space-y-6"
+                action="#"
+                onSubmit={handleSubmit}
+              >
                 <div>
                   <label
                     for="email"
@@ -33,7 +60,8 @@ const AppLogin = () => {
                     id="email"
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                     placeholder="name@company.com"
-                    required=""
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
                 <div>
@@ -50,7 +78,8 @@ const AppLogin = () => {
                     id="password"
                     placeholder="••••••••"
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                    required=""
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
                 <div class="flex items-center justify-between">
